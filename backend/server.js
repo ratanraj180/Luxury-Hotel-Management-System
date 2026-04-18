13338267ed92b0fb0c963965ab2876ac03307022
ratanraj180 <ratanraj_yogendraprasad@srmap.edu.in>
2026-04-19 01:31:26 +0530

    Configure production CORS and Environment variables

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', process.env.FRONTEND_URL],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
